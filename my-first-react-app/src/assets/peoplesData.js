6ad16ff24212391dfee33b42b6c4f755a7a661a1
Balaji642000 <balajik642000@gmail.com>
2025-04-01 07:28:25 +0530

    Completion of TourismApp

---
+++ my-first-react-app/src/assets/peoplesData.js
@@ -0,0 +1,26 @@
+export default [
+    {
+        name:"Balaji",
+        mobile: 6379151323,
+        age: 25,
+        email: "balajik642000@gmail.com"
+    },
+    {
+        name:"SivaKavin",
+        mobile: 6379151323,
+        age: 25,
+        email: "balajik642000@gmail.com"
+    },
+    {
+        name:"Indhu",
+        mobile: 6379151323,
+        age: 25,
+        email: "balajik642000@gmail.com"
+    },
+    {
+        name:"Thulasi",
+        mobile: 6379151323,
+        age: 25,
+        email: "balajik642000@gmail.com"
+    }
+]
--- my-first-react-app/src/components/home/home.jsx
+++ my-first-react-app/src/components/home/home.jsx
@@ -1,25 +1,20 @@
 import Contacts from "../contact/contacts";
 import Header from "../header/header";
 import "./home.css"
+import peoplesData from "../../assets/peoplesData";
+
 
 function Home(){
+    console.log(peoplesData);
+    const ContactDetails = peoplesData.map( (person, index) => {
+        return <Contacts key={index} name={person.name} mob={person.mobile} email={person.email} age={person.age}/>
+    })
     return(
         <>
            <Header/>
            <div className="contacts-container">
-           <Contacts name="Balaji" mob="6379151323" email="balajik642000@gmail.com" age="25" />
-           <Contacts name="SivaKavin" mob="1234567890" email="sivakavin642000@gmail.com" age="25" />
-           <Contacts name="Thulasi" mob="0987654321" email="thulasi642000@gmail.com" age="25" />
-           <Contacts name="Indhu" mob="6379151323" email="Indhu642000@gmail.com" age="25" />
-           <Contacts name="Ruba" mob="6379151323" email="Ruba642000@gmail.com" age="25" />
-           <Contacts name="Sindhu" mob="6379151323" email="sindhu642000@gmail.com" age="25" />
-           <Contacts name="Nandi" mob="6379151323" email="nandi642000@gmail.com" age="25" />
-           <Contacts name="Balaji" mob="6379151323" email="balajik642000@gmail.com" age="25" />
-           <Contacts name="Balaji" mob="6379151323" email="balajik642000@gmail.com" age="25" />
-           <Contacts name="Balaji" mob="6379151323" email="balajik642000@gmail.com" age="25" />
-           <Contacts name="Balaji" mob="6379151323" email="balajik642000@gmail.com" age="25" />
-           <Contacts name="Balaji" mob="6379151323" email="balajik642000@gmail.com" age="25" />
-           <Contacts name="Balaji" mob="6379151323" email="balajik642000@gmail.com" age="25" /> 
+           {ContactDetails}
+           {ContactDetails} 
            </div>
         </>
     )
